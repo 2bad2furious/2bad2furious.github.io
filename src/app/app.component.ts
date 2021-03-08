e31ecf8aa32f83710e67e7efc831bfb0d609fbb2
Martin Macura <mr.martin.macura@gmail.com>
2021-03-09 00:50:00 +0100

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
+import {IconDefinition} from '@fortawesome/free-solid-svg-icons';
+import {faGithub, faInstagram, faLinkedin, faMediumM} from '@fortawesome/free-brands-svg-icons';
+
+
+export interface ILink {
+  icon: IconDefinition;
+  link: string;
+  classes?: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio';
+
+  static className = 'animate__jackInTheBox';
+  @HostBinding() readonly class = 'd-flex flex-column w-100 min-vh-100';
+  readonly links: ILink[][] = [
+    // {icon: faTwitter, link: 'https://twitter.com/2bad2furious', classes: 'twitter', text: 'Dog stuff'},
+    [{
+      icon: faMediumM,
+      link: 'https://2bad2furious.medium.com/',
+      classes: 'medium',
+      text: 'I sometimes get the urge to write about something I found either cool or weird'
+    }, {icon: faGithub, link: 'https://github.com/2bad2furious', text: 'I do sometimes build stuff'}],
+    [{icon: faInstagram, link: 'https://www.instagram.com/2qte2fabulous/', text: 'Watch me get ripped'},
+      {icon: faLinkedin, link: 'https://www.linkedin.com/in/martin-macura-41442013b/', text: 'Watch me get professional'}]
+  ];
+  hasNavBorder!: boolean;
+  private browserDark = true;
+  private customScheme?: boolean;
+  private timeout?: ReturnType<typeof setTimeout>;
+
+  constructor() {
+    const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+    this.browserDark = isDarkTheme.matches;
+    isDarkTheme.addEventListener('change', e => this.browserDark = e.matches);
+
+    const hasNavBorder = window.matchMedia('(min-width: 992px)');
+    this.hasNavBorder = !hasNavBorder.matches;
+    hasNavBorder.addEventListener('change', e => this.hasNavBorder = !e.matches);
+  }
+
+  @HostBinding('class.bg-dark') get isDark(): boolean {
+    return this.customScheme ?? this.browserDark;
+  }
+
+  @HostBinding('class.bg-light') get isLight(): boolean {
+    return !this.isDark;
+  }
+
+  private static resetAnimationClass(el: HTMLElement): void {
+    el.classList.remove(AppComponent.className);
+  }
+
+  switch(switcher: HTMLElement): void {
+    clearTimeout(this.timeout);
+    switcher.classList.remove(AppComponent.className);
+    setTimeout(() => switcher.classList.add(AppComponent.className));
+    this.customScheme = !this.isDark;
+    this.timeout = setTimeout(() => AppComponent.resetAnimationClass(switcher), 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
