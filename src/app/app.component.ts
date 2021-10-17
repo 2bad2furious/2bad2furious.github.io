386b7a70840006c2b430fb9ed06752b4e294686f
Martin Macura <martin.macura@guided.cz>
2021-10-17 11:43:55 +0200

    local storage for scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
-import {Component, HostBinding, HostListener} from '@angular/core';
-import {IconDefinition} from '@fortawesome/free-solid-svg-icons';
+import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
+import {faMoon, faSun, IconDefinition} from '@fortawesome/free-solid-svg-icons';
 import {faGithub, faInstagram, faLinkedin, faMediumM} from '@fortawesome/free-brands-svg-icons';
+import {BehaviorSubject, combineLatest, Observable} from 'rxjs';
+import {map, take} from 'rxjs/operators';
 
+export interface IScheme {
+  isDark: boolean;
+  isLight: boolean;
+  color: 'light' | 'dark';
+  invertedColor: 'light' | 'dark';
+  switcherIcon: IconDefinition;
+}
 
 export interface ILink {
   icon: IconDefinition;
@@ -15,7 +24,23 @@ export interface ILink {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  static readonly darkScheme: IScheme = {
+    isDark: true,
+    isLight: false,
+    color: 'dark',
+    invertedColor: 'light',
+    switcherIcon: faSun
+  };
+  static readonly lightScheme: IScheme = {
+    isDark: false,
+    isLight: true,
+    color: 'light',
+    invertedColor: 'dark',
+    switcherIcon: faMoon
+  };
+  static readonly LOCAL_STORAGE_KEY = 'scheme';
 
   readonly links: ILink[][] = [
     [{
@@ -27,28 +52,57 @@ export class AppComponent {
     [{icon: faInstagram, link: 'https://www.instagram.com/2qte2fabulous/', text: 'Watch me get ripped'},
       {icon: faLinkedin, link: 'https://www.linkedin.com/in/martin-macura-41442013b/', text: 'Watch me get professional'}]
   ];
-  private browserDark = true;
-  private customSchemeDark?: boolean;
 
-  constructor() {
+  private readonly localStorageSubject = new BehaviorSubject<'light' | 'dark' | null>(null);
+  private readonly systemPreferenceSubject = new BehaviorSubject<'light' | 'dark' | null>(null);
+
+  private readonly darkObservable = combineLatest([this.localStorageSubject, this.systemPreferenceSubject])
+    .pipe(map(([local, system]) => {
+      return (local ?? system ?? 'dark') === 'dark';
+    }));
+  readonly schemeObservable: Observable<IScheme> = this.darkObservable
+    .pipe(map(v => v ? AppComponent.darkScheme : AppComponent.lightScheme));
+
+  constructor(
+    private readonly e: ElementRef<HTMLElement>
+  ) {
     const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
-    this.browserDark = isDarkTheme.matches;
-    isDarkTheme.addEventListener('change', e => this.browserDark = e.matches);
+    if (isDarkTheme.matches || window.matchMedia('(prefers-color-scheme: light)')) {
+      this.systemPreferenceSubject.next(isDarkTheme.matches ? 'dark' : 'light');
+      isDarkTheme.addEventListener('change', ev => this.systemPreferenceSubject.next(ev.matches ? 'dark' : 'light'));
+    }
+
+    const localScheme = localStorage.getItem(AppComponent.LOCAL_STORAGE_KEY);
+    if (localScheme === 'light') {
+      this.localStorageSubject.next('light');
+    } else if (localScheme === 'dark') {
+      this.localStorageSubject.next('dark');
+    }
+
+    this.setBgColorAsync();
   }
 
-  @HostBinding('class.bg-dark') get isDark(): boolean {
-    return this.customSchemeDark ?? this.browserDark;
+  ngOnInit(): void {
+    setTimeout(() => document.body.classList.remove('inactive'), 500);
   }
 
-  @HostBinding('class.bg-light') get isLight(): boolean {
-    return !this.isDark;
-  }
-
-  switch(): void {
-    this.customSchemeDark = !this.isDark;
+  async switch(): Promise<void> {
+    const v = await this.schemeObservable.pipe(take(1)).toPromise();
+    this.localStorageSubject.next(v.invertedColor);
+    localStorage.setItem(AppComponent.LOCAL_STORAGE_KEY, v.invertedColor);
+    this.setBgColor(v.invertedColor);
   }
 
   @HostListener('window:resize') resized(): void {
     // no need to do anything, I just needed to force the redraw
+  }
+
+  private setBgColor(scheme: 'light' | 'dark'): void {
+    this.e.nativeElement.classList.toggle('bg-light', scheme === 'light');
+    this.e.nativeElement.classList.toggle('bg-dark', scheme === 'dark');
+  }
+
+  private async setBgColorAsync(): Promise<void> {
+    this.setBgColor((await this.schemeObservable.pipe(take(1)).toPromise()).color);
   }
 }
